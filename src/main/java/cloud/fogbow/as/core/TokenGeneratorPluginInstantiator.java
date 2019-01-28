c23a501e7d8e96da36d469738c90279fb664b5fb
fubica <fubica@dsc.ufcg.edu.br>
2019-01-28 12:49:06 -0300

    Created a copy of PluginFactory because we cannot use the common version due to different class loaders

--- src/main/java/cloud/fogbow/as/core/TokenGeneratorPluginInstantiator.java
+++ src/main/java/cloud/fogbow/as/core/TokenGeneratorPluginInstantiator.java
@@ -1,6 +1,5 @@
 package cloud.fogbow.as.core;
 
-import cloud.fogbow.common.util.PluginFactory;
 import cloud.fogbow.as.core.constants.ConfigurationConstants;
 import cloud.fogbow.as.core.tokengenerator.TokenGeneratorPlugin;
 
