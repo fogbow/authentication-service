05ec79af3a362e843d5d5a14a595795abe460549
Paulo Ewerton <pauloewerton@lsd.ufcg.edu.br>
2019-03-08 10:39:30 -0300

    Adding cookie header to cloudstacksystemuser
    this data is needed for all of the cloudstack requests

--- src/main/java/cloud/fogbow/as/core/models/CloudStackSystemUser.java
+++ src/main/java/cloud/fogbow/as/core/models/CloudStackSystemUser.java
@@ -2,8 +2,17 @@ package cloud.fogbow.as.core.models;
 
 import cloud.fogbow.common.models.CloudStackUser;
 
+import java.util.HashMap;
+
 public class CloudStackSystemUser extends OneToOneMappableSystemUser {
+    private HashMap<String, String> cookieHeader;
+
     public CloudStackSystemUser(String identityProviderId, CloudStackUser cloudUser) {
         super(cloudUser.getId(), cloudUser.getName(), identityProviderId, cloudUser.getToken());
+        this.cookieHeader = cloudUser.getCookieHeader();
+    }
+
+    public HashMap<String, String> getCookieHeader() {
+        return cookieHeader;
     }
 }
