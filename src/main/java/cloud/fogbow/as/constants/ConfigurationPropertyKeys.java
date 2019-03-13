23705fe7dcf1be22ed9bf61dfb6eb7a902de9072
wassoares <wlfsoares@gmail.com>
2019-03-13 15:45:39 -0300

    Fixes made in the OpenNebulaSystemIdentityProviderPlugin class

--- src/main/java/cloud/fogbow/as/constants/ConfigurationPropertyKeys.java
+++ src/main/java/cloud/fogbow/as/constants/ConfigurationPropertyKeys.java
@@ -9,6 +9,7 @@ public class ConfigurationPropertyKeys {
     public static final String SYSTEM_IDENTITY_PROVIDER_PLUGIN_CLASS_KEY = "system_identity_provider_plugin_class";
 
     // Federation Identity Plugins configurations
+    public static final String CLOUD_IDENTITY_PROVIDER_URL_KEY = "cloud_identity_provider_url";
     public static final String CLOUDSTACK_URL_KEY = "cloudstack_url";
     public static final String LDAP_BASE_KEY = "ldap_base";
     public static final String LDAP_ENDPOINT_KEY = "ldap_endpoint";
--- src/main/java/cloud/fogbow/as/core/systemidp/plugins/opennebula/OpenNebulaSystemIdentityProviderPlugin.java
+++ src/main/java/cloud/fogbow/as/core/systemidp/plugins/opennebula/OpenNebulaSystemIdentityProviderPlugin.java
@@ -6,20 +6,25 @@ import cloud.fogbow.common.exceptions.*;
 import cloud.fogbow.as.core.PropertiesHolder;
 import cloud.fogbow.common.models.OpenNebulaUser;
 import cloud.fogbow.common.plugins.cloudidp.opennebula.OpenNebulaIdentityProviderPlugin;
+import cloud.fogbow.common.util.PropertiesUtil;
+
 import org.apache.log4j.Logger;
 import cloud.fogbow.as.constants.ConfigurationPropertyKeys;
 
 import java.util.Map;
+import java.util.Properties;
 
 public class OpenNebulaSystemIdentityProviderPlugin implements SystemIdentityProviderPlugin<OpenNebulaSystemUser> {
-    private static final Logger LOGGER = Logger.getLogger(OpenNebulaSystemIdentityProviderPlugin.class);
-
+    
+	private static final Logger LOGGER = Logger.getLogger(OpenNebulaSystemIdentityProviderPlugin.class);
+    
     private OpenNebulaIdentityProviderPlugin identityProviderPlugin;
     private String identityProviderId;
 
     public OpenNebulaSystemIdentityProviderPlugin() throws FatalErrorException {
         this.identityProviderId = PropertiesHolder.getInstance().getProperty(ConfigurationPropertyKeys.LOCAL_MEMBER_ID_KEY);
-        this.identityProviderPlugin = new OpenNebulaIdentityProviderPlugin();
+    	String identityUrl = PropertiesHolder.getInstance().getProperty(ConfigurationPropertyKeys.OPENNEBULA_URL_KEY);
+        this.identityProviderPlugin = new OpenNebulaIdentityProviderPlugin(identityUrl);
     }
 
     @Override
