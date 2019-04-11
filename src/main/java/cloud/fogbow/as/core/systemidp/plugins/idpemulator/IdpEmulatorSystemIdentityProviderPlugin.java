659f1af64508f0c36cda8baa0624ab2a41fdda68
Anderson Dantas <a.kleber.d@gmail.com>
2019-04-11 14:46:16 -0300

    Rename and refactor old TestTokenGeneratorPlugin to implement SystemIdentityProviderPlugin interface

---
+++ src/main/java/cloud/fogbow/as/core/systemidp/plugins/idpemulator/IdpEmulatorSystemIdentityProviderPlugin.java
@@ -0,0 +1,46 @@
+package cloud.fogbow.as.core.systemidp.plugins.idpemulator;
+
+import cloud.fogbow.as.constants.ConfigurationPropertyKeys;
+import cloud.fogbow.as.core.PropertiesHolder;
+import cloud.fogbow.as.core.systemidp.SystemIdentityProviderPlugin;
+import cloud.fogbow.common.constants.Messages;
+import cloud.fogbow.common.exceptions.FogbowException;
+import cloud.fogbow.common.exceptions.InvalidParameterException;
+import cloud.fogbow.common.exceptions.UnauthenticatedUserException;
+import cloud.fogbow.common.models.SystemUser;
+import org.apache.log4j.Logger;
+
+import java.util.Map;
+
+public class IdpEmulatorSystemIdentityProviderPlugin implements SystemIdentityProviderPlugin<SystemUser> {
+
+    private final Logger LOGGER;
+
+    private final String USER_NAME_FIELD = "username";
+    private final String identityProviderId;
+
+    public IdpEmulatorSystemIdentityProviderPlugin() {
+        this.LOGGER = Logger.getLogger(IdpEmulatorSystemIdentityProviderPlugin.class);
+        this.identityProviderId = PropertiesHolder.getInstance().getProperty(ConfigurationPropertyKeys.LOCAL_MEMBER_ID_KEY);
+    }
+
+    @Override
+    public SystemUser getSystemUser(Map<String, String> userCredentials) throws FogbowException {
+        String username = userCredentials.get(USER_NAME_FIELD);
+
+        if (username == null) {
+            throw new InvalidParameterException(cloud.fogbow.as.constants.Messages.Exception.INVALID_CREDENTIALS);
+        }
+
+        if (!isAuthenticated(username)) {
+            LOGGER.error(Messages.Exception.AUTHENTICATION_ERROR);
+            throw new UnauthenticatedUserException();
+        }
+
+        return new SystemUser(username, username, this.identityProviderId);
+    }
+
+    private boolean isAuthenticated(String password) {
+        return password.equals("fogbow");
+    }
+}
