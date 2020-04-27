70f01860361f103c158426762a735f74e4a93114
fubica <fubica@dsc.ufcg.edu.br>
2020-04-27 17:03:20 -0300

    Fixing merge

--- src/main/java/cloud/fogbow/as/core/systemidp/plugins/aws/v2/AwsV2SystemIdentityProviderPlugin.java
+++ src/main/java/cloud/fogbow/as/core/systemidp/plugins/aws/v2/AwsV2SystemIdentityProviderPlugin.java
@@ -6,18 +6,18 @@ import cloud.fogbow.as.core.models.AwsV2SystemUser;
 import cloud.fogbow.as.core.systemidp.SystemIdentityProviderPlugin;
 import cloud.fogbow.common.exceptions.FogbowException;
 import cloud.fogbow.common.models.AwsV2User;
-import cloud.fogbow.common.plugins.cloudidp.aws.v2.AwsV2IdentityProviderPlugin;
+import cloud.fogbow.common.plugins.cloudidp.aws.v2.AwsIdentityProviderPlugin;
 
 import java.util.Map;
 
 public class AwsV2SystemIdentityProviderPlugin implements SystemIdentityProviderPlugin<AwsV2SystemUser> {
 
-    private AwsV2IdentityProviderPlugin identityProviderPlugin;
+    private AwsIdentityProviderPlugin identityProviderPlugin;
     private String identityProviderId;
 
     public AwsV2SystemIdentityProviderPlugin() {
         this.identityProviderId = PropertiesHolder.getInstance().getProperty(ConfigurationPropertyKeys.PROVIDER_ID_KEY);
-        this.identityProviderPlugin = new AwsV2IdentityProviderPlugin();
+        this.identityProviderPlugin = new AwsIdentityProviderPlugin();
     }
 
     @Override
