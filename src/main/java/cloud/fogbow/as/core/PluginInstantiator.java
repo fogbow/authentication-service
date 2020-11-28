2423baae5ca30f4adf2cd4bc89c9755e934fe0e9
Armstrong Goes <armstrongmsg@gmail.com>
2020-11-27 22:46:20 -0300

    Removed role attribution function from AuthenticationService

--- src/main/java/cloud/fogbow/as/core/PluginInstantiator.java
+++ src/main/java/cloud/fogbow/as/core/PluginInstantiator.java
@@ -1,8 +1,6 @@
 package cloud.fogbow.as.core;
 
 import cloud.fogbow.as.constants.ConfigurationPropertyKeys;
-import cloud.fogbow.as.core.role.SystemRolePlugin;
-import cloud.fogbow.as.core.role.plugins.DefaultSystemRolePlugin;
 import cloud.fogbow.as.core.systemidp.SystemIdentityProviderPlugin;
 
 public class PluginInstantiator {
@@ -11,14 +9,5 @@ public class PluginInstantiator {
     public static SystemIdentityProviderPlugin getSystemIdentityProviderPlugin() {
         String className = PropertiesHolder.getInstance().getProperty(ConfigurationPropertyKeys.SYSTEM_IDENTITY_PROVIDER_PLUGIN_CLASS_KEY);
         return (SystemIdentityProviderPlugin) PluginInstantiator.classFactory.createPluginInstance(className);
-    }
-    
-    public static SystemRolePlugin getSystemRolePlugin() {
-        if (PropertiesHolder.getInstance().getProperties().containsKey(ConfigurationPropertyKeys.SYSTEM_ROLE_PLUGIN_CLASS_KEY)) {
-            String className = PropertiesHolder.getInstance().getProperty(ConfigurationPropertyKeys.SYSTEM_ROLE_PLUGIN_CLASS_KEY);
-            return (SystemRolePlugin) PluginInstantiator.classFactory.createPluginInstance(className);
-        } else {
-            return new DefaultSystemRolePlugin();
-        }
     }
 }
